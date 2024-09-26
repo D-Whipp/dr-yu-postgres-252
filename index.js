aa5d3d42dc68e81557ef5f73877a3d725f5c7cb1
Mr Whipp <dwhipp88@gmail.com>
2024-09-26 17:32:50 -0600

    re-installed pgadmin and updated packages, completed lesson by setting up db

--- index.js
+++ index.js
@@ -1,43 +1,70 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
+import 'dotenv/config';
+import pg from 'pg';
+
+// environment variables
+const postgresUser = process.env.POSTGRES_USER;
+const postgresPW = process.env.POSTGRES_PASSWORD;
+
+const db = new pg.Client({
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+});
+
+db.connect();
+
+db.query('SELECT * FROM capitals', (err, res) => {
+  if (err) {
+    console.error('Error executing query', err.stack);
+  } else {
+    quiz = res.rows;
+  }
+
+  db.end();
+});
 
 const app = express();
 const port = 3000;
 
 let quiz = [
-  { country: "France", capital: "Paris" },
-  { country: "United Kingdom", capital: "London" },
-  { country: "United States of America", capital: "New York" },
+  { country: 'France', capital: 'Paris' },
+  { country: 'United Kingdom', capital: 'London' },
+  { country: 'United States of America', capital: 'New York' },
 ];
 
 let totalCorrect = 0;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 let currentQuestion = {};
 
 // GET home page
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   totalCorrect = 0;
   await nextQuestion();
   console.log(currentQuestion);
-  res.render("index.ejs", { question: currentQuestion });
+  res.render('index.ejs', { question: currentQuestion });
 });
 
 // POST a new post
-app.post("/submit", (req, res) => {
+app.post('/submit', (req, res) => {
   let answer = req.body.answer.trim();
   let isCorrect = false;
-  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+  if (
+    currentQuestion.capital.toLowerCase() === answer.toLowerCase()
+  ) {
     totalCorrect++;
     console.log(totalCorrect);
     isCorrect = true;
   }
 
   nextQuestion();
-  res.render("index.ejs", {
+  res.render('index.ejs', {
     question: currentQuestion,
     wasCorrect: isCorrect,
     totalScore: totalCorrect,
